2949578f5dfe9c7ae508a11f75d7fbf0deb72c89
yogkri <dharmayog91@gmail.com>
2021-09-27 23:38:29 +0530

    Adding Generics Class To Take Any DataType

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,13 +1,21 @@
 package com.bridgelabz;
 
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a1, a2, a3;
+
+    public FindMaxValue(T a1, T a2, T a3) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
 
     /**
-     * Method used to compare three integer values and get the maximum value.
-     * a1 : First value to compare
-     * a2 : Second value to compare
-     * a3 : Third value to compare
-     * @return max : Maximum of three integers
+     * Generic method to compare any type of data and find maximum
+     * @param a1 : First value to compare
+     * @param a2 : Second value to compare
+     * @param a3 : Third value to compare
+     * @return max : Maximum of three values
      */
 
     public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
@@ -19,12 +27,10 @@ public class FindMaxValue {
         return max;
     }
 
-
-
     public static void main(String[] args) {
-        System.out.println("Welcome To Find Maximum Problem Using Generics \n");
-        System.out.println("The maximum value between the three integer is : " + maxOfValues(10, 20, 5));
-        System.out.println("The maximum value between the three float is : " + maxOfValues(3.87f, 7.56f, 5.987f));
-        System.out.println("The maximum value between the three string is : " + maxOfValues("Yogendra", "Lalit", "Sharma"));
+        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+        System.out.println("The maximum value between the three integer is : " + maxOfValues(3, 8, 5)+ "\n");
+        System.out.println("The maximum value between the three float is : " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+        System.out.println("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "xyz"));
     }
 }
