86bd605edcafd122465aa28da5b5a63a6c3fccbc
yogkri <dharmayog91@gmail.com>
2021-09-27 23:20:02 +0530

    To Find Maximum Float

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -19,8 +19,20 @@ public class FindMaxValue {
         return max;
     }
 
+    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
+        Float max = f1;
+        if (f2.compareTo(max) > 0)
+            max = f2;
+        if (f3.compareTo(max) > 0)
+            max = f3;
+        return max;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome To Find Maximun Problem Using Generics \n");
         System.out.println("The maximum value between the three integer is : " + maxOfInteger(10, 20, 5));
+        System.out.println("The maximum value between the three float is : " + maxOfFloat(3.87f, 7.56f, 5.987f));
     }
 }
