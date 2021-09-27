09457cf9899c48e683b4acef4d06114b47f886d9
yogkri <dharmayog91@gmail.com>
2021-09-27 23:31:35 +0530

    To Compare And Find Maximum Of DataTypes

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -10,8 +10,8 @@ public class FindMaxValue {
      * @return max : Maximum of three integers
      */
 
-    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3){
-        Integer max = a1;
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+        T max = a1;
         if (a2.compareTo(max) > 0)
             max = a2;
         if (a3.compareTo(max) > 0)
@@ -19,28 +19,12 @@ public class FindMaxValue {
         return max;
     }
 
-    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
-        Float max = f1;
-        if (f2.compareTo(max) > 0)
-            max = f2;
-        if (f3.compareTo(max) > 0)
-            max = f3;
-        return max;
-    }
-    public static String maxOfString(String s1, String s2, String s3) {
-        String max = s1;
-        if (s2.compareTo(max) > 0)
-            max = s2;
-        if (s3.compareTo(max) > 0)
-            max = s3;
-        return max;
-    }
 
 
     public static void main(String[] args) {
         System.out.println("Welcome To Find Maximum Problem Using Generics \n");
-        System.out.println("The maximum value between the three integer is : " + maxOfInteger(10, 20, 5));
-        System.out.println("The maximum value between the three float is : " + maxOfFloat(3.87f, 7.56f, 5.987f));
-        System.out.println("The maximum value between the three string is : " + maxOfString("Yogendra", "Lalit", "Sharma"));
+        System.out.println("The maximum value between the three integer is : " + maxOfValues(10, 20, 5));
+        System.out.println("The maximum value between the three float is : " + maxOfValues(3.87f, 7.56f, 5.987f));
+        System.out.println("The maximum value between the three string is : " + maxOfValues("Yogendra", "Lalit", "Sharma"));
     }
 }
